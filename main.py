6ded73710654c250ecc3f1d7ede17cdebca3ed85
Allegui <32678906+Allegui@users.noreply.github.com>
2022-03-25 00:47:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/beginning')
-async def say_hello():
-    return "Hi I'm Guillaume ! I'm using FastAPI."
+@app.get('/beginning/{name}')
+async def say_hello(name: str):
+    return f"Hello, My name is {name} and I'm using Fast API."
