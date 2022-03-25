e68f49dbb64227ebcc99e8f54df8e705e25caeae
Allegui <32678906+Allegui@users.noreply.github.com>
2022-03-25 02:10:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,14 +21,29 @@ pickle_in = open("xgb_cl_undersampling.pkl","rb")
 xgb_cl_undersampling = pickle.load(pickle_in)
 
 
+
+@app.get("/predict/{name}")
+async def predict(name: int):
+  
+    return f"My name is {name}"
+
+
+
+
+
+
+
+
+
+
 # ML API endpoint for making prediction aganist the request received from client
 
   
-@app.get("/predict/{identifiant}")
-async def predict(identifiant: int):
+# @app.get("/predict/{identifiant}")
+# async def predict(identifiant: int):
     
-    data_df = test_X.loc[test_X.index==np.asscalar(SK_ID_CURR_test_X.loc[SK_ID_CURR_test_X['SK_ID_CURR']==identifiant].index),:]
+#     data_df = test_X.loc[test_X.index==np.asscalar(SK_ID_CURR_test_X.loc[SK_ID_CURR_test_X['SK_ID_CURR']==identifiant].index),:]
     
-    # Create prediction
-    prediction = float(xgb_cl_undersampling.predict_proba(data_df)[0][1])
-    return {"prediction": prediction}
+#     # Create prediction
+#     prediction = float(xgb_cl_undersampling.predict_proba(data_df)[0][1])
+#     return {"prediction": prediction}
