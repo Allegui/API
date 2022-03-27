e408f287e38c93fb440bbe479497de58241c3208
Allegui <32678906+Allegui@users.noreply.github.com>
2022-03-27 20:37:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,20 +22,6 @@ xgb_cl_undersampling = joblib.load('xgb_cl_undersampling.joblib')
 # # ML API endpoint for making prediction aganist the request received from client
 
 
-# # /{identifiant}
-
-# @app.get("/predict")
-# async def predict(identifiant: int):
-    
-#     data_df = test_X.loc[test_X.index==np.asscalar(SK_ID_CURR_test_X.loc[SK_ID_CURR_test_X['SK_ID_CURR']==identifiant].index),:]
-    
-#     # Create prediction
-#     score = float(np.array(xgb_cl_undersampling.predict_proba(data_df),dtype='float64')[0][1])
-  
-# #     score = np.array(data_df)[0][4]
-
-#     return {"prediction": str(score)}
-
 
 def predict(resp) : 
     score = xgb_cl_undersampling.predict_proba(resp)[0][1]    
