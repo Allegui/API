5866fb6967c797052e006c88c2ae645df5cf257d
Allegui <32678906+Allegui@users.noreply.github.com>
2022-03-27 19:48:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,19 +22,30 @@ xgb_cl_undersampling = joblib.load('xgb_cl_undersampling.joblib')
 # # ML API endpoint for making prediction aganist the request received from client
 
 
-# /{identifiant}
+# # /{identifiant}
 
-@app.get("/predict")
-async def predict(identifiant: int):
+# @app.get("/predict")
+# async def predict(identifiant: int):
     
-    data_df = test_X.loc[test_X.index==np.asscalar(SK_ID_CURR_test_X.loc[SK_ID_CURR_test_X['SK_ID_CURR']==identifiant].index),:]
+#     data_df = test_X.loc[test_X.index==np.asscalar(SK_ID_CURR_test_X.loc[SK_ID_CURR_test_X['SK_ID_CURR']==identifiant].index),:]
     
-    # Create prediction
-    score = float(np.array(xgb_cl_undersampling.predict_proba(data_df),dtype='float64')[0][1])
+#     # Create prediction
+#     score = float(np.array(xgb_cl_undersampling.predict_proba(data_df),dtype='float64')[0][1])
   
-#     score = np.array(data_df)[0][4]
+# #     score = np.array(data_df)[0][4]
 
-    return {"prediction": str(score)}
+#     return {"prediction": str(score)}
+
+
+def predict(resp) : 
+    score = xgb_cl_undersampling.predict_proba(resp)[0][1]    
+    return {"score" : str(score)}
+    
+
+@app.get('/get_predict')
+async def get_predict(identifiant: int) :
+    resp = test_X.loc[test_X.index==np.asscalar(SK_ID_CURR_test_X.loc[SK_ID_CURR_test_X['SK_ID_CURR']==identifiant].index),:]
+    return predict(resp)
   
   
 if __name__ == '__main__' : 
